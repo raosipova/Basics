9b1c93678082aa7689ea9997ec071a0c8c9045df
Regina <OsRigli87@mail.ru>
2024-04-15 19:09:52 +0500

    fix CI

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -2,7 +2,8 @@ package ru.netology;
 
 import org.testng.annotations.Test;
 
-import static org.testng.AssertJUnit.assertEquals;
+
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
